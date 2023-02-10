cd9245c74c781deb11314dc8eabbbab5d14c8a48
travis <alextavern@gmail.com>
2023-02-10 14:31:23 +0100

    final minor changes

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from torch import nn
 import torchvision.models as models
+import torch.nn.functional as F
 from helper import conv2d_output_size
 
 
@@ -71,15 +72,56 @@ class MLP1(nn.Module):
         return predictions
 
 
-class CNNScratch-ESC(nn.Module):
-    def __init__(self, input_shape):
-        super().__init__():
-        self.input_shape = input_shape
+class CNN3(nn.Module):
+    def __init__(self, input_shape, num_cats=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.conv5 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(128)
+        self.conv6 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm2d(128)
+        self.conv7 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.bn7 = nn.BatchNorm2d(256)
+        self.conv8 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.bn8 = nn.BatchNorm2d(256)
+        self.dense1 = nn.Linear(256*(((input_shape[0]//2)//2)//2)*(((input_shape[1]//2)//2)//2), 500)
+        self.dropout = nn.Dropout(0.5)
+        self.dense2 = nn.Linear(500, num_cats)
 
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(self.bn1(x))
+        x = self.conv2(x)
+        x = F.relu(self.bn2(x))
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv3(x)
+        x = F.relu(self.bn3(x))
+        x = self.conv4(x)
+        x = F.relu(self.bn4(x))
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv5(x)
+        x = F.relu(self.bn5(x))
+        x = self.conv6(x)
+        x = F.relu(self.bn6(x))
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv7(x)
+        x = F.relu(self.bn7(x))
+        x = self.conv8(x)
+        x = F.relu(self.bn8(x))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.dense1(x))
+        x = self.dropout(x)
+        x = self.dense2(x)
+        return x
 
-        )
+
 class CNNScratch1(nn.Module):
 
     def __init__(self, dropout, flattened_size):
@@ -284,25 +326,3 @@ class DenseNet(nn.Module):
         output = self.model(x)
         return output
 
-
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(RNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, self.hidden_size, self.num_layers, batch_first=True, dropout=0.2)
-        # self.gru = nn.GRU(input_size, self.hidden_size, self.num_layers, batch_first=True, dropout=0.2)
-        self.fc1 = nn.Linear(hidden_size, int(hidden_size / 2))
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(int(hidden_size / 2), int(hidden_size / 2))
-        self.fc3 = nn.Linear(int(hidden_size / 2), num_classes)
-
-    def forward(self, x):
-        x = x.float()
-        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size).float()).cuda()
-        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size).float()).cuda()
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.relu(self.fc1(out[:, -1, :]))
-        out = self.relu(self.fc2(out))
-        out = self.fc3(out)
-        return out
